cd40789c6e4feca9c73594a6dd02ac4b37c19754
MaikonDias <maikon@DESKTOP-FLUOHRG>
2024-08-31 15:53:59 -0300

    [CHORE] Initial commit

---
+++ lib/soundcloud_tui.ex
@@ -0,0 +1,18 @@
+defmodule SoundcloudTui do
+  @moduledoc """
+  Documentation for `SoundcloudTui`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SoundcloudTui.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/soundcloud_tui_test.exs
@@ -0,0 +1,8 @@
+defmodule SoundcloudTuiTest do
+  use ExUnit.Case
+  doctest SoundcloudTui
+
+  test "greets the world" do
+    assert SoundcloudTui.hello() == :world
+  end
+end
